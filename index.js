177886a161bcf167833dca5aeb3f1b7003a3a33b
aebb27 <arielbatista37@gmail.com>
2021-03-24 10:18:52 -0500

    DRY JS

--- index.js
+++ index.js
@@ -3,25 +3,13 @@ const showMore = document.querySelectorAll('.show-more-buttons'),
 	faqAnswer = document.querySelectorAll('.question-answer');
 
 function showInfo(index) {
-	if (index === 0) {
-		showMore[index].classList.add('hide');
-		showLess[index].classList.remove('hide');
-		faqAnswer[index].classList.remove('hide');
-	} else {
-		showMore[index].classList.add('hide');
-		showLess[index].classList.remove('hide');
-		faqAnswer[index].classList.remove('hide');
-	}
+	showMore[index].classList.add('hide');
+	showLess[index].classList.remove('hide');
+	faqAnswer[index].classList.remove('hide');
 }
 
 function hideInfo(index) {
-	if (index === 0) {
-		showMore[index].classList.remove('hide');
-		showLess[index].classList.add('hide');
-		faqAnswer[index].classList.add('hide');
-	} else {
-		showMore[index].classList.remove('hide');
-		showLess[index].classList.add('hide');
-		faqAnswer[index].classList.add('hide');
-	}
+	showMore[index].classList.remove('hide');
+	showLess[index].classList.add('hide');
+	faqAnswer[index].classList.add('hide');
 }
